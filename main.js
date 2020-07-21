66d0aafd8aa9cb2578bd4189098e39451bffebc0
Danium <Kividanya964@gmail.com>
2020-07-21 18:04:22 +0300

    jquery for popUp is added

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.feedback').click(function() {
+	debugger
+	$('.popUp_background').css("display", "flex")
+})
+
+$('.cancel').click(function() {
+	debugger
+	$('.popUp_background').css("display", "none")
+})
+
+$('.send').click(function() {
+	debugger
+	$('.popUp_background').css("display", "none")
+})
