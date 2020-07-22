1a2ddce9e9881b44214412710aa1b7e469ed3984
Danium <Kividanya964@gmail.com>
2020-07-22 15:38:45 +0300

    debuggers is deleted

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 $('.feedback').click(function() {
-	debugger
 	$('.popUp_background').css("display", "flex")
 })
 
 $('.cancel').click(function() {
-	debugger
 	$('.popUp_background').css("display", "none")
 })
 
 $('.send').click(function() {
-	debugger
 	$('.popUp_background').css("display", "none")
 })
